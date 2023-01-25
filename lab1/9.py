7375f2f59159b07017700ba1c64e7ee03114ea97
gaziza13 <120405381+gaziza13@users.noreply.github.com>
2023-01-25 22:26:39 +0600

    Update 9.py

--- lab1/9.py
+++ lab1/9.py
@@ -1,20 +1,11 @@
-<<<<<<< HEAD
-x=5
-x=float(x)
 
-y=5.5
-y=int(y)
-
-z=5
-z=complex(z)
-=======
 x="Hello World"
 print(len(x))
 
 #2 exercise
 y=x[0]
 
-<<<<<<<< HEAD:lab1/10.py
+
 #3 exercise
 z=x[2:5]
 
@@ -34,8 +25,3 @@ x=x.replace("H","J")
 age = 36
 txt="My name is John, and I am {}"
 print(txt.format(age))
-========
-z=5
-z=complex(z)
->>>>>>>> d0cd3c9f282332dcf2c56cfdb878fed50a7f6ac7:lab1/9.py
->>>>>>> d0cd3c9f282332dcf2c56cfdb878fed50a7f6ac7
