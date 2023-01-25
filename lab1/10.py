d2709e1fba9339e9ca02757f4e7c76db3ab22f09
gaziza13 <gazeka.04@mail.ru>
2023-01-25 21:58:24 +0600

    strings

--- lab1/10.py
+++ lab1/10.py
@@ -1,8 +1,25 @@
-x=5
-x=float(x)
+x="Hello World"
+print(len(x))
 
-y=5.5
-y=int(y)
+#2 exercise
+y=x[0]
 
-z=5
-z=complex(z)
+#3 exercise
+z=x[2:5]
+
+#4 exercise
+c=x.strip()
+
+#5 exercise
+x=x.upper()
+
+#6 exercise
+x=x.lower()
+
+#7 exercise
+x=x.replace("H","J")
+
+#8 exercise
+age = 36
+txt="My name is John, and I am {}"
+print(txt.format(age))
